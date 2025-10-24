90e02d4125af7d539b9748592f2d6dc9f7a0e4f1
Julian <julian.moik@googlemail.com>
2025-10-24 13:51:35 +0200

    script/interpreter: use std::move for OP_TOALTSTACK and OP_FROMALTSTACK, this is much more efficient and we don't have to charge any varcost for these move operations

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1493,8 +1493,7 @@ bool EvalScript(ValtypeStack& stack, const CScript& script, unsigned int flags, 
                 {
                     if (stack.size() < 1)
                         return set_error(serror, SCRIPT_ERR_INVALID_STACK_OPERATION);
-                    stackPushCosted(altstack, std::move(stacktop(-1)), varcost);
-                    popstack(stack);
+                    altstack.push_back(stack.pop_back_valtype());
                 }
                 break;
 
@@ -1502,8 +1501,7 @@ bool EvalScript(ValtypeStack& stack, const CScript& script, unsigned int flags, 
                 {
                     if (altstack.size() < 1)
                         return set_error(serror, SCRIPT_ERR_INVALID_ALTSTACK_OPERATION);
-                    stackPushCosted(stack, std::move(altstacktop(-1)), varcost);
-                    popstack(altstack);
+                    stack.push_back(altstack.pop_back_valtype());
                 }
                 break;
 
