270d3454351a4f89b4797b9d9d58187edd1f36ed
Byron Hambly <bhambly@blockstream.com>
2026-05-14 14:55:57 +0200

    add macos compat for simplicity fuzz

--- src/test/fuzz/simplicity.cpp
+++ src/test/fuzz/simplicity.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <cstdio>
+#include <compat/endian.h>
 #include <primitives/transaction.h>
 #include <script/interpreter.h>
 extern "C" {
